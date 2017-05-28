86ce915039d606eea99582c568163297d0b3c00b
Jonas Stein <news@jonasstein.de>
2017-05-28 13:28:01 +0200

    save to .csv file

--- speed.py
+++ speed.py
@@ -8,7 +8,21 @@ Created on Fri May 26 22:01:16 2017
 
 import datetime
 
-def measure(Distance_meter, FzID):
+def CreateLogfile():
+    MyLogTime = datetime.datetime.now()
+    MyFileName = MyLogTime.strftime("protokoll_%Y-%m-%dT%H_%M_%S.csv")
+    f = open(MyFileName, 'w')
+    f.write('"Nr.","Datum Uhrzeit","Dauer (s)","Geschwindigkeit (km/h)"\n')
+    f.close
+    return(MyFileName)
+
+def LogThis(MyFileName, Text):
+    f = open(MyFileName, 'a')
+    f.write(Text + '\n')
+    f.close
+
+
+def measure(Distance_meter, FzID, MyFileName):
     Tstart = datetime.datetime.now()
     input("# (Messung läuft...)                              Stopp mit Return")
     Tstop = datetime.datetime.now()
@@ -20,21 +34,26 @@ def measure(Distance_meter, FzID):
     StrTimeStamp = Tstart.strftime("%Y-%m-%d %H:%M:%S")
  
     print('%03d, %s, %.6f,   %.1f'%(FzID,StrTimeStamp,TdifferenceSeconds,Speed_km_h))
-    
+    LogThis(MyFileName,'%03d,%s,%.6f,%.1f'%(FzID,StrTimeStamp,TdifferenceSeconds,Speed_km_h))
 
 def main():
     FzID = 0
+
+    MyLogFile = CreateLogfile()
+
     Distance_meter = 86.5
     print('Geschwindigkeitsmessung.\n Distanz zwischen Start- und Stoppsignal: %f (m)\n\n'%Distance_meter)
     print('Nr., Datum       Uhrzeit, Dauer (s),  Geschwindigkeit (km/h)')
     while True:
         KeyByUser = input("# (Standby)                                       Start mit Return, Quit mit q")
         if KeyByUser != '':
-            print('ciao.\n')
+            print('Ende der Messserie.\n')
+            print('Protokoll öffnen mit')
+            print('libreoffice %s'%(MyLogFile))
             break
         elif KeyByUser == '':
             FzID = FzID + 1
-            measure(Distance_meter, FzID)
+            measure(Distance_meter, FzID, MyLogFile)
        
 
 if __name__ == '__main__':
